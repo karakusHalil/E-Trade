75b5815af18734366925465624b4c8a43ac58f4f
karakusHalil <karakus_halil@outlook.com>
2025-06-09 02:15:37 +0300

    Add policy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Footer from "./components/Layout/footer/Footer";
 import Header from "./components/Layout/header/Header";
+import Policy from "./components/Layout/policy/Policy";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Policy />
       <Footer />
     </div>
   );
