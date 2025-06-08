1ac6ac0f3ac3e41eb9351a4ed5d1eb87b470e395
karakusHalil <karakus_halil@outlook.com>
2025-06-09 02:52:06 +0300

    Add products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Policy from "./components/Layout/policy/Policy";
 import Sliders from "./components/Sliders/Sliders";
 import "./App.css";
 import Categories from "./components/Categories/Categories";
+import Products from "./components/Products/Products";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Sliders />
       <Categories />
+      <Products />
       <Policy />
       <Footer />
     </div>
