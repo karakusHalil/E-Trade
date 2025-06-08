6e1560b34c7dd4e95c31e849e760049ad6d32e5d
karakusHalil <karakus_halil@outlook.com>
2025-06-09 02:26:18 +0300

    Add sliders

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
-import "./App.css";
 import Footer from "./components/Layout/footer/Footer";
 import Header from "./components/Layout/header/Header";
 import Policy from "./components/Layout/policy/Policy";
+import Sliders from "./components/Sliders/Sliders";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Sliders />
       <Policy />
       <Footer />
     </div>
