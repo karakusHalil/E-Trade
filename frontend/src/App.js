24ee7a964f5f7895853b5e617c7e301ea2f6a0f8
karakusHalil <karakus_halil@outlook.com>
2025-06-09 02:02:53 +0300

    Add footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Footer from "./components/Layout/footer/Footer";
 import Header from "./components/Layout/header/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
