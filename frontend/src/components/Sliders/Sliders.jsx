dd19b851397f7d43f40962aba336aca66c7e4972
karakusHalil <karakus_halil@outlook.com>
2025-06-09 02:30:29 +0300

    Add SliderItem

--- frontend/src/components/Sliders/Sliders.jsx
+++ frontend/src/components/Sliders/Sliders.jsx
@@ -1,23 +1,13 @@
 import React from "react";
 import "./Sliders.css";
+import SliderItem from "./SliderItem";
 
 const Sliders = () => {
   return (
     <>
       <section className="slider">
         <div className="slider-elements">
-          <div className="slider-item fade">
-            <div className="slider-image">
-              <img src="img/slider/slider1.jpg" className="img-fluid" alt />
-            </div>
-            <div className="container">
-              <p className="slider-title">SUMMER 2022</p>
-              <h2 className="slider-heading">Save up to 70%</h2>
-              <a href="#" className="btn btn-lg btn-primary">
-                Explore Now
-              </a>
-            </div>
-          </div>
+          <SliderItem />
           {/* <div className="slider-item fade">
             <div className="slider-image">
               <img src="img/slider/slider2.jpg" className="img-fluid" alt />
