4d6615483da052fc754fc65c055f6fd34a0fb648
karakusHalil <karakus_halil@outlook.com>
2025-06-10 01:31:59 +0300

    code review

--- frontend/src/components/Products/Products.jsx
+++ frontend/src/components/Products/Products.jsx
@@ -4,201 +4,33 @@ import ProductItem from "./ProductItem";
 const Products = () => {
   return (
     <>
-      <section className="products">
-        <div className="container">
-          <div className="section-title">
-            <h2>New Arrivals</h2>
+      <section class="products">
+        <div class="container">
+          <div class="section-title">
+            <h2>Featured Products</h2>
             <p>Summer Collection New Morden Design</p>
           </div>
-          <div className="product-wrapper product-carousel2">
-            <div className="glide__track">
-              <ul className="product-list glide__slides">
+          <div class="product-wrapper product-carousel">
+            <div class="glide__track">
+              <ul class="product-list glide__slides" id="product-list">
                 <ProductItem />
-                <li className="product-item glide__slide">
-                  <div className="product-image">
-                    <a href="#">
-                      <img
-                        src="img/products/product2/1.png"
-                        alt
-                        className="img1"
-                      />
-                      <img
-                        src="img/products/product2/2.png"
-                        alt
-                        className="img2"
-                      />
-                    </a>
-                  </div>
-                  <div className="product-info">
-                    <a href="$" className="product-title">
-                      Analogue Resin Strap
-                    </a>
-                    <ul className="product-star">
-                      <li>
-                        <i className="bi bi-star-fill" />
-                      </li>
-                      <li>
-                        <i className="bi bi-star-fill" />
-                      </li>
-                      <li>
-                        <i className="bi bi-star-fill" />
-                      </li>
-                      <li>
-                        <i className="bi bi-star-fill" />
-                      </li>
-                      <li>
-                        <i className="bi bi-star-half" />
-                      </li>
-                    </ul>
-                    <div className="product-prices">
-                      <strong className="new-price">$108.00</strong>
-                      <span className="old-price">$165.00</span>
-                    </div>
-                    <span className="product-discount">-17%</span>
-                    <div className="product-links">
-                      <button>
-                        <i className="bi bi-basket-fill" />
-                      </button>
-                      <button>
-                        <i className="bi bi-heart-fill" />
-                      </button>
-                      <a href="#">
-                        <i className="bi bi-eye-fill" />
-                      </a>
-                      <a href="#">
-                        <i className="bi bi-share-fill" />
-                      </a>
-                    </div>
-                  </div>
-                </li>
-                <li className="product-item glide__slide">
-                  <div className="product-image">
-                    <a href="#">
-                      <img
-                        src="img/products/product3/1.png"
-                        alt
-                        className="img1"
-                      />
-                      <img
-                        src="img/products/product3/2.png"
-                        alt
-                        className="img2"
-                      />
-                    </a>
-                  </div>
-                  <div className="product-info">
-                    <a href="$" className="product-title">
-                      Analogue Resin Strap
-                    </a>
-                    <ul className="product-star">
-                      <li>
-                        <i className="bi bi-star-fill" />
-                      </li>
-                      <li>
-                        <i className="bi bi-star-fill" />
-                      </li>
-                      <li>
-                        <i className="bi bi-star-fill" />
-                      </li>
-                      <li>
-                        <i className="bi bi-star-fill" />
-                      </li>
-                      <li>
-                        <i className="bi bi-star-half" />
-                      </li>
-                    </ul>
-                    <div className="product-prices">
-                      <strong className="new-price">$108.00</strong>
-                      <span className="old-price">$165.00</span>
-                    </div>
-                    <span className="product-discount">-17%</span>
-                    <div className="product-links">
-                      <button>
-                        <i className="bi bi-basket-fill" />
-                      </button>
-                      <button>
-                        <i className="bi bi-heart-fill" />
-                      </button>
-                      <a href="#">
-                        <i className="bi bi-eye-fill" />
-                      </a>
-                      <a href="#">
-                        <i className="bi bi-share-fill" />
-                      </a>
-                    </div>
-                  </div>
-                </li>
-                <li className="product-item">
-                  <div className="product-image">
-                    <a href="#">
-                      <img
-                        src="img/products/product4/1.png"
-                        alt
-                        className="img1"
-                      />
-                      <img
-                        src="img/products/product4/2.png"
-                        alt
-                        className="img2"
-                      />
-                    </a>
-                  </div>
-                  <div className="product-info">
-                    <a href="$" className="product-title">
-                      Analogue Resin Strap
-                    </a>
-                    <ul className="product-star">
-                      <li>
-                        <i className="bi bi-star-fill" />
-                      </li>
-                      <li>
-                        <i className="bi bi-star-fill" />
-                      </li>
-                      <li>
-                        <i className="bi bi-star-fill" />
-                      </li>
-                      <li>
-                        <i className="bi bi-star-fill" />
-                      </li>
-                      <li>
-                        <i className="bi bi-star-half" />
-                      </li>
-                    </ul>
-                    <div className="product-prices">
-                      <strong className="new-price">$108.00</strong>
-                      <span className="old-price">$165.00</span>
-                    </div>
-                    <span className="product-discount">-17%</span>
-                    <div className="product-links">
-                      <button>
-                        <i className="bi bi-basket-fill" />
-                      </button>
-                      <button>
-                        <i className="bi bi-heart-fill" />
-                      </button>
-                      <a href="#">
-                        <i className="bi bi-eye-fill" />
-                      </a>
-                      <a href="#">
-                        <i className="bi bi-share-fill" />
-                      </a>
-                    </div>
-                  </div>
-                </li>
+                <ProductItem />
+                <ProductItem />
+                <ProductItem />
               </ul>
             </div>
-            <div className="glide__arrows">
+            <div class="glide__arrows">
               <button
-                className="glide__arrow glide__arrow--left"
+                class="glide__arrow glide__arrow--left"
                 data-glide-dir="<"
               >
-                <i className="bi bi-chevron-left" />
+                <i class="bi bi-chevron-left"></i>
               </button>
               <button
-                className="glide__arrow glide__arrow--right"
+                class="glide__arrow glide__arrow--right"
                 data-glide-dir=">"
               >
-                <i className="bi bi-chevron-right" />
+                <i class="bi bi-chevron-right"></i>
               </button>
             </div>
           </div>
