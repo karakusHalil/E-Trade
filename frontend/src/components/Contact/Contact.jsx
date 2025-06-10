6a0dd3d74e48c84e16e0e52f28ff915a4f7a513e
karakusHalil <karakus_halil@outlook.com>
2025-06-11 00:19:51 +0300

    Refactor: Split Contact section into separate components

--- frontend/src/components/Contact/Contact.jsx
+++ frontend/src/components/Contact/Contact.jsx
@@ -1,21 +1,13 @@
 import "./Contact.css";
+import ContactForm from "./ContactForm";
+import ContactMap from "./ContactMap";
 
 const Contact = () => {
   return (
     <>
       <section className="contact">
         <div className="contact-top">
-          <div className="contact-map">
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3008.9633698339308!2d28.929441087738052!3d41.04793012296828!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cab1d021adf417%3A0xba3a3fdfdbb5f5d!2sEy%C3%BCp%20Sultan%20Camii!5e0!3m2!1str!2str!4v1665091191675!5m2!1str!2str"
-              width="100%"
-              height={500}
-              style={{ border: 0 }}
-              allowFullScreen
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-            />
-          </div>
+          <ContactMap />
         </div>
         <div className="contact-bottom">
           <div className="container">
@@ -30,45 +22,7 @@ const Contact = () => {
               </p>
             </div>
             <div className="contact-elements">
-              <form className="contact-form">
-                <div className>
-                  <label>
-                    Your Name
-                    <span>*</span>
-                  </label>
-                  <input type="text" required />
-                </div>
-                <div className>
-                  <label>
-                    Your email
-                    <span>*</span>
-                  </label>
-                  <input type="text" required />
-                </div>
-                <div className>
-                  <label>
-                    Subject
-                    <span>*</span>
-                  </label>
-                  <input type="text" required />
-                </div>
-                <div className>
-                  <label>
-                    Your message
-                    <span>*</span>
-                  </label>
-                  <textarea
-                    id="author"
-                    name="author"
-                    type="text"
-                    value
-                    size={30}
-                    required
-                    defaultValue={""}
-                  />
-                </div>
-                <button className="btn btn-sm form-button">Send Message</button>
-              </form>
+              <ContactForm />
               <div className="contact-info">
                 <div className="contact-info-item">
                   <div className="contact-info-texts">
