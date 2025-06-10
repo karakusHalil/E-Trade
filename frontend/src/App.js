822f4db4560bbbd05512e206449336e66f3529fb
karakusHalil <karakus_halil@outlook.com>
2025-06-10 19:03:32 +0300

    Add shop page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import Home from "./pages/Home";
 import "./App.css";
+import Shop from "./pages/Shop";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Shop />
     </div>
   );
 }
--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -1,12 +1,19 @@
-import React from "react";
 import Header from "../components/Layout/header/Header";
 import Policy from "../components/Layout/policy/Policy";
 import Footer from "../components/Layout/footer/Footer";
+import Categories from "../components/Categories/Categories";
+import Products from "../components/Products/Products";
+import CampaignSingle from "../components/CampaignSingle/CampaignSingle";
+import ProductSingle from "../components/Products/ProductSingle";
 
 const Shop = () => {
   return (
     <>
       <Header />
+      <Categories />
+      <Products />
+      <CampaignSingle />
+      <ProductSingle />
       <Policy />
       <Footer />
     </>
