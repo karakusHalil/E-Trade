481813159b6fdb9741435b0fbbc42b7c947e7031
karakusHalil <karakus_halil@outlook.com>
2025-06-11 03:59:22 +0300

    Add reviews

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
 import BlogList from "./pages/BlogList";
-import "./App.css";
 import ContactPage from "./pages/ContactPage";
 import Auth from "./components/Auth/Auth";
 import Account from "./pages/Account";
 import ProductDetail from "./pages/ProductDetail";
 import ShoppingCart from "./pages/ShoppingCart";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <ShoppingCart />
+      <ProductDetail />
     </div>
   );
 }
