a9fd4995ab8bd8154a308bd46fa5758e22f44479
karakusHalil <karakus_halil@outlook.com>
2025-06-11 03:25:53 +0300

    Code review

--- frontend/src/components/ProductDetail/Gallery/ProductGallery.jsx
+++ frontend/src/components/ProductDetail/Gallery/ProductGallery.jsx
@@ -8,7 +8,38 @@ const ProductGallery = () => {
         </div>
         <div className="product-thumb">
           <div className="glide__track" data-glide-el="track">
-            <ol className="gallery-thumbs glide__slides" />
+            <ol className="gallery-thumbs glide__slides">
+              <li
+                className="glide__slide glide__slide--active"
+                style={{ width: 109, marginRight: 5 }}
+              >
+                <img
+                  src="img/products/product2/1.png"
+                  alt
+                  className="img-fluid active"
+                />
+              </li>
+              <li
+                className="glide__slide"
+                style={{ width: 109, marginLeft: 5, marginRight: 5 }}
+              >
+                <img
+                  src="img/products/product2/2.png"
+                  alt
+                  className="img-fluid"
+                />
+              </li>
+              <li
+                className="glide__slide"
+                style={{ width: 109, marginLeft: 5 }}
+              >
+                <img
+                  src="img/products/product2/3.png"
+                  alt
+                  className="img-fluid"
+                />
+              </li>
+            </ol>
           </div>
           <div className="glide__arrows" data-glide-el="controls">
             <button
