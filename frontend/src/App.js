c493dbcceb748ccd871be4888b5b4a60007b8462
karakusHalil <karakus_halil@outlook.com>
2025-06-11 04:10:50 +0300

    Add single blog

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,13 @@ import Auth from "./components/Auth/Auth";
 import Account from "./pages/Account";
 import ProductDetail from "./pages/ProductDetail";
 import ShoppingCart from "./pages/ShoppingCart";
+import SingleBlog from "./pages/SingleBlog";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <ProductDetail />
+      <SingleBlog />
     </div>
   );
 }
---
+++ frontend/src/pages/SingleBlog.jsx
@@ -0,0 +1,17 @@
+import Footer from "../components/Layout/footer/Footer";
+import Header from "../components/Layout/header/Header";
+import Policy from "../components/Layout/policy/Policy";
+import BlogDetail from "../components/BlogDetail/BlogDetail";
+
+const SingleBlog = () => {
+  return (
+    <>
+      <Header />
+      <BlogDetail />
+      <Policy />
+      <Footer />
+    </>
+  );
+};
+
+export default SingleBlog;
