26864b4e502c3e1bf0c02e77a4775d47336babc1
karakusHalil <karakus_halil@outlook.com>
2025-06-13 01:27:36 +0300

    Add Product schema

--- backend/Models/Product.js
+++ backend/Models/Product.js
@@ -1,4 +1,18 @@
 const mongoose = require("mongoose");
 
+const ProductSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    images: { type: Array, default: [] },
+    price: { type: Number, default: 0.0 },
+    description: { type: String, default: "" },
+    colors: { type: Array, default: [] },
+    sizes: { type: Array, default: [] },
+    stockCode: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
-const ProductSchema = mongoose.Scheme
+const Product = mongoose.model("Product", ProductSchema);
+
+module.exports = Product;
