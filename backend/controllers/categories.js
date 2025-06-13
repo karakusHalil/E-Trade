e63437affc9914a05ad51e4516411208fe4c1411
karakusHalil <karakus_halil@outlook.com>
2025-06-13 18:13:43 +0300

    Implement all CRUD operations for Categories API

--- backend/controllers/categories.js
+++ backend/controllers/categories.js
@@ -17,7 +17,7 @@ router.post("/", async (req, res) => {
 
 //CREATE CATEGORY END
 
-//GET CATEGORİES START
+//GET ALL CATEGORİES START
 
 router.get("/", async (req, res) => {
   try {
@@ -28,10 +28,65 @@ router.get("/", async (req, res) => {
   }
 });
 
-//GET CATEGORİES END
+//GET ALL CATEGORİES END
 
+//GET CATEGORY BY ID START
 
+router.get("/:categoryId", async (req, res) => {
+  try {
+    const categoryId = req.params.categoryId;
+    try {
+      const category = await Category.findById(categoryId);
+      res.status(200).json(category);
+    } catch (error) {
+      res.status(404).json({ error: "Kategori bulunamadı !" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Sunucu Hatası !" });
+  }
+});
 
+//GET CATEGORY BY ID END
 
+//PUT CATEGORY START
+
+router.put("/:categoryId", async (req, res) => {
+  try {
+    const categoryId = req.params.categoryId;
+    const updateInfo = req.body;
+
+    const category = await Category.findById(categoryId);
+    if (!category) {
+      return res.status(404).json({ error: "Kategori Bulunamadı !" });
+    }
+    const updateCategory = await Category.findByIdAndUpdate(
+      categoryId,
+      updateInfo,
+      { new: true }
+    );
+    res.status(200).json(updateCategory);
+  } catch (error) {
+    res.status(500).json({ error: "Sunucu Hatası !" });
+  }
+});
+
+//PUT CATEGORY END
+
+//DELETE CATEGORY START
+
+router.delete("/:categoryId", async (req, res) => {
+  try {
+    const categoryId = req.params.categoryId;
+    const deletedCategory = await Category.findByIdAndDelete(categoryId);
+    if (!deletedCategory) {
+      return res.status(404).json({ error: "Kategori Bulunamadı !" });
+    }
+    res.status(200).json(deletedCategory);
+  } catch (error) {
+    res.status(500).json({ error: "Sunucu Hatası !" });
+  }
+});
+
+//DELETE CATEGORY END
 
 module.exports = router;
