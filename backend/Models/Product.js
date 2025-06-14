79476767069471263e89eb01491de78d1754522a
karakusHalil <karakus_halil@outlook.com>
2025-06-14 22:15:22 +0300

    Code review

--- backend/Models/Product.js
+++ backend/Models/Product.js
@@ -2,13 +2,14 @@ const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
+    name: { type: String, required: true, trim: true },
     images: { type: Array, default: [] },
-    price: { type: Number, default: 0.0 },
+    price: { type: Number, require: true, min: 0 },
     description: { type: String, default: "" },
     colors: { type: Array, default: [] },
     sizes: { type: Array, default: [] },
-    stockCode: { type: String, required: true },
+    stockCode: { type: String, required: true, unique: true },
+    isActive: { type: Boolean, default: true },
   },
   { timestamps: true }
 );
