cda7636abab6ed291d3bf6e987364c986e9c53dc
karakusHalil <karakus_halil@outlook.com>
2025-06-16 03:44:39 +0300

    Add post login routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,8 +4,8 @@ const users = require("../controllers/users");
 
 router.get("/", users.getAllUsers);
 router.get("/:userId", users.getUserById);
-router.post("/", users.createUser);
+router.post("/register", users.createUser);
+router.post("/login", users.loginUser);
 router.put("/:userId", users.updateUser);
 router.delete("/:userId", users.deleteUser);
-
 module.exports = router;
