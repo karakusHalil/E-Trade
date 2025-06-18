ea612dcaad2d77135fa09495be0e4d1211b903f0
karakusHalil <karakus_halil@outlook.com>
2025-06-19 00:37:44 +0300

    Created list with ant design

--- frontend/src/pages/Admin/Category/CategoryList.jsx
+++ frontend/src/pages/Admin/Category/CategoryList.jsx
@@ -1,12 +1,67 @@
-import "./CategoryList.css"
+import { Space, Table, Tag, Button } from "antd";
+import "./CategoryList.css";
 
-
+const columns = [
+  {
+    title: "Image",
+    dataIndex: "image",
+    key: "img",
+    with: "25%",
+    render: (imgUrl) => <img src={imgUrl} alt="Category Image" />,
+  },
+  {
+    title: "Category Name",
+    dataIndex: "name",
+    key: "name",
+    with: "50%",
+    align: "center",
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: "Process",
+    key: "process",
+    with: "%25",
+    render: (record) => (
+      <>
+        <Button color="cylan" variant="solid" style={{ marginRight: "5px" }}>
+          Update
+        </Button>
+        <Button color="danger" variant="solid">
+          Remove
+        </Button>
+      </>
+    ),
+  },
+];
+const data = [
+  {
+    key: "1",
+    name: "John Brown",
+    age: 32,
+    address: "New York No. 1 Lake Park",
+    tags: ["nice", "developer"],
+  },
+  {
+    key: "2",
+    name: "Jim Green",
+    age: 42,
+    address: "London No. 1 Lake Park",
+    tags: ["loser"],
+  },
+  {
+    key: "3",
+    name: "Joe Black",
+    age: 32,
+    address: "Sydney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+  },
+];
 const CategoryList = () => {
   return (
     <>
-    
+      <Table columns={columns} dataSource={data} />;
     </>
-  )
-}
+  );
+};
 
-export default CategoryList
+export default CategoryList;
