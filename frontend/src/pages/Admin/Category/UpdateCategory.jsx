979ef40723a77d24ce5ebdb59951568a12f9f98f
karakusHalil <karakus_halil@outlook.com>
2025-06-19 00:44:20 +0300

    Created update category with ant design

--- frontend/src/pages/Admin/Category/UpdateCategory.jsx
+++ frontend/src/pages/Admin/Category/UpdateCategory.jsx
@@ -1,12 +1,44 @@
-import "./UpdateCategory.css"
-
+import { Button, Form, Input } from "antd";
+import "./UpdateCategory.css";
 
 const UpdateCategory = () => {
   return (
     <>
-    
-    </>
-  )
-}
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        initialValues={{ remember: true }}
+        // onFinish={onFinish}
+        // onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Category Name"
+          name="username"
+          rules={[
+            { required: true, message: "Please input your Category Name!" },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
-export default UpdateCategory
+        <Form.Item
+          name="img"
+          label="Image Url"
+          rules={[{ required: true, message: "Please input your Image Url!" }]}
+        >
+          <Input.TextArea />
+        </Form.Item>
+
+        <Form.Item label={null}>
+          <Button type="primary" htmlType="submit">
+            Update
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
+};
+
+export default UpdateCategory;
