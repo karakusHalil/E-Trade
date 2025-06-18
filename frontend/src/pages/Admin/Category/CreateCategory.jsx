9b61659e36744a7b87ec07dcf5921c0583592ada
karakusHalil <karakus_halil@outlook.com>
2025-06-18 03:07:24 +0300

    Add category create

---
+++ frontend/src/pages/Admin/Category/CreateCategory.jsx
@@ -0,0 +1,37 @@
+import { Button, Form, Input } from "antd";
+import "./CreateCategories.css";
+const CreateCategory = () => {
+  return (
+    <>
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        initialValues={{ remember: true }}
+        // onFinish={onFinish}
+        // onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: "Please input your username!" }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item name={["user", "introduction"]} label="Introduction">
+          <Input.TextArea />
+        </Form.Item>
+
+        <Form.Item label={null}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
+};
+
+export default CreateCategory;
