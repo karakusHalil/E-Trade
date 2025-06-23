715dd2bb6a6c5a365538f2c62ada5a8fb828b060
karakusHalil <karakus_halil@outlook.com>
2025-06-24 01:35:39 +0300

    UpdateCategory completed by id

--- frontend/src/pages/Admin/Category/UpdateCategory.jsx
+++ frontend/src/pages/Admin/Category/UpdateCategory.jsx
@@ -1,29 +1,78 @@
 import { Button, Form, Input } from "antd";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import "./UpdateCategory.css";
+import { useEffect } from "react";
 
 const UpdateCategory = () => {
+  const [form] = Form.useForm();
   const navigate = useNavigate();
+  const params = useParams();
+  const categoryId = params.id;
+  console.log(categoryId);
+  const getCategoryById = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:5100/api/categories/${categoryId}`
+      );
+      if (response.ok) {
+        const data = await response.json();
+        if (data) {
+          form.setFieldsValue({
+            name: data.name,
+            img: data.img,
+            id: categoryId,
+          });
+          console.log(form.getFieldValue);
+        }
+      }
+    } catch (error) {
+      console.log("Sunucu Hatası !");
+    }
+  };
+
+  useEffect(() => {
+    getCategoryById();
+  }, []);
+
+  const handleUpdateCategory = async (values) => {
+    try {
+      const response = await fetch(
+        `http://localhost:5100/api/categories/${categoryId}`,
+        {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(values),
+        }
+      );
+      if (response.ok) {
+        console.log("Kategori Güncellendi !");
+      } else {
+        console.log("Kategori Güncellenirken Hata Oluştu !");
+      }
+    } catch (error) {
+      console.log("Sunucu Hatası !");
+    }
+  };
   return (
     <>
       <div className="category-update-wrapper">
         <Form
+          form={form}
           name="basic"
           labelCol={{ span: 8 }}
           wrapperCol={{ span: 16 }}
-          initialValues={{ remember: true }}
-          // onFinish={onFinish}
+          onFinish={handleUpdateCategory}
           // onFinishFailed={onFinishFailed}
           autoComplete="off"
         >
           <Form.Item
             label="Category Name"
-            name="username"
+            name="name"
             rules={[
               { required: true, message: "Please input your Category Name!" },
             ]}
           >
-            <Input />
+            <Input placeholder="Category Name enter..." />
           </Form.Item>
 
           <Form.Item
@@ -33,7 +82,7 @@ const UpdateCategory = () => {
               { required: true, message: "Please input your Image Url!" },
             ]}
           >
-            <Input.TextArea />
+            <Input placeholder="Category Image enter..." />
           </Form.Item>
 
           <Form.Item label={null}>
