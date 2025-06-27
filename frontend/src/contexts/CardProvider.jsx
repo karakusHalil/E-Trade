205cdeca0555e0158e1adc443c6906017198973b
karakusHalil <karakus_halil@outlook.com>
2025-06-27 20:02:43 +0300

    Created context card provider

---
+++ frontend/src/contexts/CardProvider.jsx
@@ -0,0 +1,24 @@
+import { createContext, useState } from "react";
+
+const CartContext = createContext();
+const CardProvider = ({ children }) => {
+  const [cartItems, setCartItems] = useState([]);
+  const addToCart = (cartItem) => {
+    setCartItems([...cartItems, cartItem]);
+  };
+  return (
+    <>
+      <CartContext.Provider
+        value={{
+          addToCart: addToCart,
+          cartItems: cartItems,
+          setCartItems: setCartItems,
+        }}
+      >
+        {children}
+      </CartContext.Provider>
+    </>
+  );
+};
+
+export default CardProvider;
