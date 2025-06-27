2c13a6de10d705555337f482f256829a067d87b4
karakusHalil <karakus_halil@outlook.com>
2025-06-27 20:05:45 +0300

    Wrap entire app with CartProvider context for global cart state management

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,15 +4,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Layout } from "./Layouts/Layout.jsx";
+import CartProvider from "./contexts/CartProvider.jsx";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Layout>
-        <App />
-      </Layout>
+      <CartProvider>
+        <Layout>
+          <App />
+        </Layout>
+      </CartProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
