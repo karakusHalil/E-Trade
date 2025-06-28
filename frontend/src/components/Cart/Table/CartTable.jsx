5502f32b9d147d2081126d4be0b9c160d4f6f3d0
karakusHalil <karakus_halil@outlook.com>
2025-06-28 20:11:34 +0300

    add product to cartTable

--- frontend/src/components/Cart/Table/CartTable.jsx
+++ frontend/src/components/Cart/Table/CartTable.jsx
@@ -1,4 +1,8 @@
+import { useContext } from "react";
+import { CartContext } from "../../../contexts/CartProvider";
+
 const CartTable = () => {
+  const { cartItems } = useContext(CartContext);
   return (
     <>
       <table className="shop-table">
@@ -13,28 +17,23 @@ const CartTable = () => {
           </tr>
         </thead>
         <tbody className="cart-wrapper">
-          <tr className="cart-item">
-            <td></td>
-            <td className="cart-image">
-              <img src="img/products/product1/1.png" alt="" />
-              <i className="bi bi-x delete-cart" data-id="1"></i>
-            </td>
-            <td>Analogue Resign Strap</td>
-            <td>$108.00</td>
-            <td className="product-quantity">1</td>
-            <td className="product-subtotal">$108.00</td>
-          </tr>
-          <tr className="cart-item">
-            <td></td>
-            <td className="cart-image">
-              <img src="img/products/product2/1.png" alt="" />
-              <i className="bi bi-x delete-cart" data-id="2"></i>
-            </td>
-            <td>Ridley High Waist</td>
-            <td>$100.00</td>
-            <td className="product-quantity">1</td>
-            <td className="product-subtotal">$100.00</td>
-          </tr>
+          {cartItems.map((product) => {
+            return (
+              <tr className="cart-item">
+                <td></td>
+                <td className="cart-image">
+                  <img src={product.images[0]} alt="" />
+                  <i className="bi bi-x delete-cart" data-id="1"></i>
+                </td>
+                <td>{product.name}</td>
+                <td>
+                  ${product.price - product.price * (product.discount / 100)}
+                </td>
+                <td className="product-quantity">1</td>
+                <td className="product-subtotal">$108.00</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </>
