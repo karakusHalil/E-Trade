7ba4b0b2bda11d8e897307249ed0a94023026fcf
karakusHalil <karakus_halil@outlook.com>
2025-07-01 01:23:00 +0300

    Add coupon routes and CRUD

---
+++ backend/controllers/coupon.js
@@ -0,0 +1,89 @@
+const CouponRepository = require("../repositories/CouponRepository.js");
+
+//Create Coupon
+
+const createCoupon = async (req, res) => {
+  try {
+    const { code, discount, expired, count } = req.body;
+    if (!code || !discount || !expired || !count) {
+      return res.status(400).json({ error: "Zorunlu alanları doldurun !" });
+    }
+    const newCoupon = await CouponRepository.create({
+      code,
+      discount,
+      expired,
+      count,
+    });
+    res.status(201).json(newCoupon);
+  } catch (error) {
+    res.status(500).json({ error: "Sunucu Hatası !" });
+  }
+};
+
+//GetAll Coupon
+
+const getAllCoupons = async (req, res) => {
+  try {
+    const coupons = await CouponRepository.getAll();
+    res.status(200).json(coupons);
+  } catch (error) {
+    res.status(500).json({ error: "Sunucu Hatası !" });
+  }
+};
+
+//GetCouponByID
+
+const getCouponById = async (req, res) => {
+  try {
+    const couponId = req.params.couponId;
+    const coupon = await CouponRepository.getById(couponId);
+    if (!coupon) {
+      return res.status(404).json("Coupon Bulunamadı ! ");
+    }
+    res.status(200).json(coupon);
+  } catch (error) {
+    res.status(500).json({ error: "Sunucu Hatası !" });
+  }
+};
+
+// UPDATE COUPON
+
+const updateCoupon = async (req, res) => {
+  try {
+    const couponId = req.params.couponId;
+    const updateData = req.body;
+    const updatedCoupon = await CouponRepository.update(
+      couponId,
+      updateData,
+    );
+    if (!updatedCoupon) {
+      return res.status(404).json({ error: "Coupon bulunamadı !" });
+    }
+    res.status(200).json(updatedCoupon);
+  } catch (error) {
+    res.status(500).json({ error: "Sunucu Hatası !" });
+  }
+};
+
+// Delete Coupon
+
+const deleteCoupon = async (req, res) => {
+  try {
+    const couponId = req.params.couponId;
+    const deletedCoupon = await CouponRepository.delete(couponId);
+    if (!deletedCoupon) {
+      return res.status(404).json({ error: "Kupon bulunamadı !" });
+    }
+    res.status(200).json(deletedCoupon);
+  } catch (error) {
+    res.status(500).json({ error: "Sunucu Hatası !" });
+  }
+};
+
+module.exports = {
+  getAllCoupons,
+  getCouponById,
+  createCoupon,
+  updateCoupon,
+  deleteCoupon,
+};
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,11 +7,13 @@ const categoryRoutes = require("./categoryRoutes");
 const productRoutes = require("./productRoutes");
 const userRoutes = require("./userRoutes");
 const authRoutes = require("./authRoutes");
+const couponRoutes = require("./couponRoutes");
 //Define Routes path
 
 router.use("/categories", categoryRoutes);
 router.use("/products", productRoutes);
 router.use("/users", userRoutes);
 router.use("/auth", authRoutes);
+router.use("/coupons", couponRoutes);
 
 module.exports = router;
