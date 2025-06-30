01e4bb9c5b2fc564b81edd835563e1fa2c6402c2
karakusHalil <karakus_halil@outlook.com>
2025-06-30 18:57:26 +0300

    Completed coupon repository

---
+++ backend/repositories/CouponRepository.js
@@ -0,0 +1,15 @@
+const Coupon = require("../models/Coupon");
+
+const CouponRepository = {
+  getAll: () => Coupon.find(),
+  getById: (couponId) => Coupon.findById(),
+  create: (couponData) => Coupon.create(couponData),
+  update: (couponId, updateData) =>
+    Coupon.findByIdAndUpdate(couponId, updateData, {
+      new: true,
+      runValidators: true,
+    }),
+  delete: (couponId) => Coupon.findByIdAndDelete(couponId),
+};
+
+module.exports = CouponRepository;
