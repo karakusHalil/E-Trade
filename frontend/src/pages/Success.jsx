617c989d3f1f6ea5964f2c67ffcf4f502d5432ae
karakusHalil <karakus_halil@outlook.com>
2025-07-08 19:22:31 +0300

    cartClear function executed on success page

--- frontend/src/pages/Success.jsx
+++ frontend/src/pages/Success.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { useEffect, useContext } from "react";
+import { CartContext } from "../contexts/CartProvider";
+import { message } from "antd";
 import { Link } from "react-router-dom";
 
 const Success = () => {
+  const { clearCart } = useContext(CartContext);
+
+  useEffect(() => {
+    clearCart(); // sepeti temizle
+    message.success("Payment completed successfully! Cart cleared.");
+  }, []);  
   return (
     <div style={{ textAlign: "center", padding: "100px 20px" }}>
       <h1>✅ Payment Successful!</h1>
